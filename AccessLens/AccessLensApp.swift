f80dab34fea17636f60d783ff6bbea30d449d275
Anoushka Wayangankar <anoushkawayangankar@gmail.com>
2026-09-03 18:02:33 +0530

    Initial Commit

---
+++ AccessLens/AccessLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessLensApp.swift
+//  AccessLens
+//
+//  Created by Anoushka Wayangankar on 03/09/26.
+//
+
+import SwiftUI
+
+@main
+struct AccessLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
